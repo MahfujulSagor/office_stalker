9fa73cd724416688088961f73540d94b6ffc0320
MahfujulSagor <mahfujulsagor@gmail.com>
2026-07-04 01:12:45 +0600

    add svg images

--- app/components/room.tsx
+++ app/components/room.tsx
@@ -1,24 +1,62 @@
+import Image from "next/image";
+import Fan from "@/public/fan.svg";
+import DrawingRoom from "@/public/drawingroom.svg";
+import WorkRoom from "@/public/workroom.svg";
 
 type RoomProps = {
-    roomType: "drawing" | "work1" | "work2"; 
-}
+  roomType: "drawing" | "work1" | "work2";
+};
 
-const Room = ({roomType} : RoomProps) => {
-  const room = roomType === "drawing" ? "Drawing Room" : roomType == "work1" ? "Work Room 1" : "Work Room 2"; // For now hardcoded, but can be dynamic based on props or state in the future.
+const Room = ({ roomType }: RoomProps) => {
+  const room =
+    roomType === "drawing"
+      ? "Drawing Room"
+      : roomType == "work1"
+        ? "Work Room 1"
+        : "Work Room 2"; // For now hardcoded, but can be dynamic based on props or state in the future.
   return (
-    <div className="flex-1 flex flex-col border border-slate-800 rounded-md p-3 relative">
+    <div className="flex-1 flex flex-col rounded-md p-3 relative shadow-lg">
       <h2 className="text-sm font-semibold mb-2">{room}</h2>
 
       {/* DEVICE AREA */}
-      <div className="flex-1 relative h-full border border-slate-800 rounded-sm">
+      <div className="flex-1 relative h-full border  rounded-sm">
         {/* LIGHTS */}
-        <div className="absolute top-8 left-8 w-6 h-6 bg-slate-600 rounded-full" />
-        <div className="absolute top-8 right-8 w-6 h-6 bg-slate-600 rounded-full" />
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 w-6 h-6 bg-slate-600 rounded-full" />
+        {/* 1 */}
+        <div className="illuminate absolute top-20 left-20 w-6 h-6 rounded-full" />
+        {/* 2 */}
+        <div className="illuminate absolute top-20 right-20 w-6 h-6 rounded-full" />
+        {/* 3 */}
+        <div className="illuminate absolute bottom-12 left-1/2 -translate-x-1/2 w-6 h-6 rounded-full" />
 
         {/* FANS */}
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-8 border border-slate-600 rounded-sm" />
-        <div className="absolute bottom-30 left-1/2 -translate-x-1/2 w-8 h-8 border border-slate-600 rounded-sm" />
+        {/* 1 */}
+        <div className="absolute top-1/5 left-1/2 -translate-x-1/2 -translate-y-1/2 w-28 h-28">
+          <Image src={Fan} alt="Fan1" className="w-full h-full animate-spin" />
+        </div>
+        {/* 2 */}
+        <div className="absolute bottom-30 left-1/2 -translate-x-1/2 w-28 h-28">
+          <Image src={Fan} alt="Fan2" className="w-full h-full animate-spin" />
+        </div>
+
+        {/* FURNITURE PLACEHOLDER */}
+        {roomType === "drawing" ? (
+          <div className="absolute -z-1 min-w-full h-full flex justify-center items-center">
+            <Image
+              src={DrawingRoom}
+              alt="Drawing Room"
+              className="w-full h-[60%] object-fit"
+            />
+          </div>
+        ) : (
+          <div className="absolute -z-1 min-w-full h-full flex justify-center items-center">
+            <Image
+              src={WorkRoom}
+              alt="Work Room"
+              className="w-full h-[50%] object-fit"
+            />
+          </div>
+        )}
+
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Room from "./components/room";
 
 export default function Page() {
   return (
-    <div className="h-screen w-full bg-slate-950 text-slate-200 flex flex-col">
+    <div className="h-screen w-full  flex flex-col">
       {/* HEADER */}
-      <header className="h-14 border-b border-slate-800 flex items-center px-6">
+      <header className="h-14 border-b  flex items-center px-6">
         <h1 className="text-lg font-semibold tracking-wide">
           Office Monitoring Dashboard
         </h1>
@@ -26,36 +26,24 @@ export default function Page() {
         </div>
 
         {/* RIGHT SIDEBAR */}
-        <div className="w-[320px] border-l border-slate-800 p-3 flex flex-col gap-3">
-          <div className="h-24 border border-slate-800 rounded p-2">
-            Device Status
-          </div>
+        <div className="w-[320px] border-l  p-3 flex flex-col gap-3">
+          <div className="h-24 border  rounded p-2">Device Status</div>
 
-          <div className="h-24 border border-slate-800 rounded p-2">
-            Power Usage
-          </div>
+          <div className="h-24 border  rounded p-2">Power Usage</div>
 
-          <div className="h-24 border border-slate-800 rounded p-2">Alerts</div>
+          <div className="h-24 border rounded p-2">Alerts</div>
 
-          <div className="flex-1 border border-slate-800 rounded p-2">
-            Room Summary
-          </div>
+          <div className="flex-1 border  rounded p-2">Room Summary</div>
         </div>
       </div>
 
       {/* BOTTOM PANEL */}
-      <div className="h-40 border-t border-slate-800 flex gap-3 p-3">
-        <div className="flex-1 border border-slate-800 rounded p-2">
-          Graph Placeholder
-        </div>
+      <div className="h-40 border-t  flex gap-3 p-3">
+        <div className="flex-1 border  rounded p-2">Graph Placeholder</div>
 
-        <div className="flex-1 border border-slate-800 rounded p-2">
-          Timeline Placeholder
-        </div>
+        <div className="flex-1 border  rounded p-2">Timeline Placeholder</div>
 
-        <div className="flex-1 border border-slate-800 rounded p-2">
-          Logs Placeholder
-        </div>
+        <div className="flex-1 border  rounded p-2">Logs Placeholder</div>
       </div>
     </div>
   );
